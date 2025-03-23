2d8172ee27bf1df1ace83af3aa673513e36c3225
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2025-03-23 18:28:07 -0400

    Create vpn-server.py

---
+++ Tunnel/vpn-server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import fcntl
+import struct
+import os
+import time
+import socket
+from scapy.all import IP
+from scapy.all import *
+
+TUNSETIFF = 0x400454ca
+IFF_TUN = 0x0001
+IFF_TAP = 0x0002
+IFF_NO_PI = 0x1000
+
+# Create the tun interface
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pack('16sH', b'tun%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+
+# Get the interface name
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface Name: {}".format(ifname))
+
+# Set up the IP address and bring the interface up
+os.system("ip addr add 192.168.53.100/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
+
+
+IP_A = "0.0.0.0"
+PORT = 9090
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP_A, PORT))
+while True:
+    data, (ip, port) = sock.recvfrom(2048)
+    print("{}:{} --> {}:{}".format(ip, port, IP_A, PORT))
+    pkt = IP(data)
+    print(" Inside: {} --> {}".format(pkt.src, pkt.dst))
+    os.write(tun, data)
